193021c88d128392c3d18f39617dae654148f2b2
Shenbagapriya Bakthavachalam <bshenbagapriya@gmail.com>
2021-01-20 09:32:49 +0530

    Add https protocol to fhir server

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -13,9 +13,6 @@
       >
         <v-list-item three-line>
           <v-list-item-content >
-            <!--div class="overline mb-4">
-              PATIENT
-            </div-->
             <v-list-item-title class="headline mb-1">
               {{patient.name[0].given[0]}}, {{patient.name[0].family}} 
             </v-list-item-title>
@@ -67,25 +64,9 @@
           <v-spacer></v-spacer>
           <v-toolbar-title>REMOTE PATIENT MONITORING - OBSERVATIONS</v-toolbar-title>
           <v-spacer></v-spacer>
-
-          <!--template v-slot:extension>
-            <v-tabs
-              v-model="tab"
-              align-with-title
-            >
-              <v-tabs-slider color="yellow"></v-tabs-slider>
-
-              <v-tab
-                v-for="rpmItem in rpmItems"
-                :key="rpmItem"
-              >
-                {{ rpmItem.text }}
-              </v-tab>
-            </v-tabs>
-          </template-->
         </v-toolbar>
         <v-row>
-          <v-col cols="3">
+          <v-col>
             <v-btn
               depressed
               color="primary"
@@ -94,7 +75,7 @@
               Body Temperature
             </v-btn>
           </v-col>
-          <v-col cols="3">
+          <v-col>
             <v-btn
               depressed
               color="primary"
@@ -103,7 +84,7 @@
               Body Weight
             </v-btn>
           </v-col>
-          <v-col cols="3">
+          <v-col>
             <v-btn
               depressed
               color="primary"
@@ -112,7 +93,7 @@
               Respiratory Rate
             </v-btn>
           </v-col>
-          <v-col cols="3">
+          <v-col>
             <v-btn
               depressed
               color="primary"
@@ -123,7 +104,45 @@
           </v-col>
         </v-row>
       </v-card>
-
+      <v-form
+        ref="form"
+      >
+        <v-card
+          class="mx-auto"
+          max-width="1080"
+          outlined>
+          <v-card-text class="bold text-center">
+            <span>PATIENT RPM OBSERVATIONS</span>
+          </v-card-text>
+          <v-row>
+            <v-col>
+              <v-select
+                :items="observationTypes"
+                v-model="selectedObservation"
+                label="Choose Observation"
+                dense
+              ></v-select>
+            </v-col>
+            <v-col>
+              <v-text-field
+                v-model="newValue"
+                label="Enter Value"
+                regular
+                dense
+              />
+            </v-col>
+            <v-col cols="2">
+              <v-btn
+                depressed
+                color="secondary"
+                @click="createPatientObservationRespiratoryRate(newObject)"
+              >
+                Add Record
+              </v-btn>
+            </v-col>
+          </v-row>
+        </v-card>
+      </v-form>
       <v-card
         class="mx-auto"
         max-width="1080"
@@ -205,15 +224,127 @@
 
       // Tabs
 
-        tab: null,
-        rpmItems: [],
-        items: [
-          { text: 'Body Temperature', value: 'temp' },
-          { text: 'Body Weight', value: 'calories two' },
-          { text: 'Respiratory Rate', value: 'calories three' },
-          { text: 'Heart rate', value: 'calories four' },
-        ]
+      tab: null,
+      rpmItems: [],
+      items: [
+        { text: 'Body Temperature', value: 'temp' },
+        { text: 'Body Weight', value: 'calories two' },
+        { text: 'Respiratory Rate', value: 'calories three' },
+        { text: 'Heart rate', value: 'calories four' },
+      ],
 
+      observationTypes: ['Temperature', 'Weight', 'Respiratory Rate', 'Heart Rate'],
+      selectedObservation: 'Temperature',
+      newValue: null,
+      // JSON Objects
+      temperatureCode: {
+              "coding": [
+                  {
+                      "system": "http://loinc.org",
+                      "code": "8310-5",
+                      "display": "Body temperature"
+                  }
+              ],
+              "text": "Body temperature"
+            },
+      temperatureQuantity: {
+              "value": 36.5,
+              "unit": "C",
+              "system": "http://unitsofmeasure.org",
+              "code": "Cel"
+            },
+      weightCode: {
+              "coding": [
+                  {
+                      "system": "http://loinc.org",
+                      "code": "29463-7",
+                      "display": "Body Weight"
+                  }
+              ],
+              "text": "Body weight"
+            },
+      weightQuantity: {
+              "value": 82.5,
+              "unit": "kg",
+              "system": "http://unitsofmeasure.org",
+              "code": "kg"
+            },
+      respiratoryCode: {
+              "coding": [
+                  {
+                      "system": "http://loinc.org",
+                      "code": "9279-1",
+                      "display": "Respiratory rate"
+                  }
+              ],
+              "text": "Respiratory rate"
+            },
+      respiratoryQuantity: {
+              "value": 10,
+              "unit": "/min",
+              "system": "http://unitsofmeasure.org",
+              "code": "/min"
+            },
+      heartCode: {
+              "coding": [
+                  {
+                      "system": "http://loinc.org",
+                      "code": "8867-4",
+                      "display": "Heart rate"
+                  }
+              ],
+              "text": "Heart rate"
+            },
+      heartQuantity: {
+              "value": 10,
+              "unit": "/min",
+              "system": "http://unitsofmeasure.org",
+              "code": "/min"
+            },
+
+      newObject: {
+          "resourceType": "Observation",
+          "meta": {
+              "tag": [
+                  {
+                      "code": "HDR Unit Testing"
+                  }
+              ]
+          },
+          "status": "final",
+          "category": [
+              {
+                  "coding": [
+                      {
+                          "system": "http://terminology.hl7.org/CodeSystem/observation-category",
+                          "code": "vital-signs",
+                          "display": "vital-signs"
+                      }
+                  ]
+              }
+          ],
+          "code": {
+              "coding": [
+                  {
+                      "system": "http://loinc.org",
+                      "code": "9279-1",
+                      "display": "Respiratory rate"
+                  }
+              ],
+              "text": "Respiratory rate"
+          },
+          "subject": {
+              "reference": "Patient/1741899"
+          },
+          "effectiveDateTime": "2021-01-19T10:13:12+05:30",
+          "issued": "2021-01-19T10:13:12+05:30",
+          "valueQuantity": {
+              "value": 10,
+              "unit": "/min",
+              "system": "http://unitsofmeasure.org",
+              "code": "/min"
+          }
+        }
     }),
     mounted () {
       const patientId = 1741899
@@ -247,43 +378,77 @@
         this.vitalSigns = this.heartRate
       },
       getPatient: function (patientId) {
-        const baseURI = 'http://hapi.fhir.org/baseR4/Patient/'+patientId
+        const baseURI = 'https://hapi.fhir.org/baseR4/Patient/'+patientId
         this.$http.get(baseURI)
         .then((result) => {
           this.patient = result.data
         })
       },
       getPatientObservationTemperature: function (patientId, code) {
-        const baseURI = 'http://hapi.fhir.org/baseR4/Observation?patient='+patientId+'&category=vital-signs&code='+code
+        const baseURI = 'https://hapi.fhir.org/baseR4/Observation?patient='+patientId+'&category=vital-signs&code='+code
         this.$http.get(baseURI)
         .then((result) => {
           this.bodyTemperature = result.data.entry
         })
       },
       getPatientObservationWeight: function (patientId, code) {
-        const baseURI = 'http://hapi.fhir.org/baseR4/Observation?patient='+patientId+'&category=vital-signs&code='+code
+        const baseURI = 'https://hapi.fhir.org/baseR4/Observation?patient='+patientId+'&category=vital-signs&code='+code
         this.$http.get(baseURI)
         .then((result) => {
           this.bodyWeight = result.data.entry
         })
       },
       getPatientObservationRespiratoryRate: function (patientId, code) {
-        const baseURI = 'http://hapi.fhir.org/baseR4/Observation?patient='+patientId+'&category=vital-signs&code='+code
+        const baseURI = 'https://hapi.fhir.org/baseR4/Observation?patient='+patientId+'&category=vital-signs&code='+code
         this.$http.get(baseURI)
         .then((result) => {
           this.respiratoryRate = result.data.entry
         })
       },
       getPatientObservationHeartRate: function (patientId, code) {
-        const baseURI = 'http://hapi.fhir.org/baseR4/Observation?patient='+patientId+'&category=vital-signs&code='+code
+        const baseURI = 'https://hapi.fhir.org/baseR4/Observation?patient='+patientId+'&category=vital-signs&code='+code
         this.$http.get(baseURI)
         .then((result) => {
           this.heartRate = result.data.entry
         })
       },
-      
+      createPatientObservationRespiratoryRate: function (body) {
+        if(this.newValue == null) {
+          alert("Please Enter Value")
+          return
+        }
+        var currentDate = new Date()
+        this.newObject["issued"] = currentDate.toISOString()
+        this.newObject["effectiveDateTime"] = currentDate.toISOString()
+        if(this.selectedObservation == "Temperature") {
+          this.newObject["code"] = this.temperatureCode
+          this.newObject["valueQuantity"] = this.temperatureQuantity
+        } else if(this.selectedObservation == "Weight") {
+          this.newObject["code"] = this.weightCode
+          this.newObject["valueQuantity"] = this.weightQuantity
+        } else if(this.selectedObservation == "Respiratory Rate") {
+          this.newObject["code"] = this.respiratoryCode
+          this.newObject["valueQuantity"] = this.respiratoryQuantity
+        } else if(this.selectedObservation == "Heart Rate") {
+          this.newObject["code"] = this.heartCode
+          this.newObject["valueQuantity"] = this.heartQuantity
+        } else {
+          alert("Invalid type")
+        }
+        this.newObject["valueQuantity"]["value"] = this.newValue
+        const baseURI = 'https://hapi.fhir.org/baseR4/Observation'
+        this.$http.post(baseURI, body, {
+            headers: { 
+                'Content-Type' : 'application/json' 
+            }
+        })
+        .then((result) => {
+          this.getPatientObservation(1741899)
+          alert("Created Successfully")
+        })
+      },
       getPatientObservation: function (patientId) {
-        const baseURI = 'http://hapi.fhir.org/baseR4/Observation?patient='+patientId
+        const baseURI = 'https://hapi.fhir.org/baseR4/Observation?patient='+patientId
         this.$http.get(baseURI)
         .then((result) => {
           this.vitalSigns = result.data.entry
